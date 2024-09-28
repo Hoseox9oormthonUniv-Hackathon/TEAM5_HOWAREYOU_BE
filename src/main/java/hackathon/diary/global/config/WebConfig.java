b9e01a7a4d6f5f65e72b9740f86444a4240a1e4f
zzangjyj0818 <jyj010818@naver.com>
2024-09-29 04:04:44 +0900

    fix: cors allowedOrigins edit

--- src/main/java/hackathon/diary/global/config/WebConfig.java
+++ src/main/java/hackathon/diary/global/config/WebConfig.java
@@ -8,7 +8,6 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
     @Value("${spring.infra.be}")
     private static String uri;
